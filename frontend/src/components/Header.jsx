ef60395886cf463c867dbc73bcc839f68264eb39
Varun <varun77x@gmail.com>
2024-10-29 21:23:02 +0530

    Initialising with Header comp

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+      <h1 className="text-4xl font-extrabold text-center mb-6 text-blue-600">
+        AI Recipe Generator
+      </h1>
+    );
+  }
+  
+  export default Header;
